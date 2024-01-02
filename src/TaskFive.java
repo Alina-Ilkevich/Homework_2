20c1b854d9b7c8c85f9b2aed5a298d84f554c64a
Alina-Ilkevich <alina.1qaz@gmail.com>
2024-01-02 14:37:56 +0300

    init commit

--- src/TaskFive.java
+++ src/TaskFive.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class TaskFive {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Введите целое положительное число: ");
+        System.out.print("Введите целое положительное число: ");
         int a = scan.nextInt();
-        if (a <= 0) {
-            System.out.println("Вы ввели неподходящее число!");
+        if (a > 0) {
+            int b = 0;
+            int c = 0;
+            while (b < a) {
+                b++;
+                c = c + b;
+            }
+            System.out.print("Сумма чисел равна " + c);}
+        else {
+                System.out.println("Вы ввели неподходящее число!");
+            }
         }
-        int b = 0;
-        int c = 0;
-        while (b <= a) {
-            b++;
-            c = c + b;
-            System.out.println(c);
-        }
-
     }
-}
